fd2aff2459722d616cf09e95732c9c6718f8e4cf
Zee <aizhangcmu@gmail.com>
2020-05-20 11:22:47 -0400

    Implement week2 LR_with_NN assignment 1-5

--- NNandDeepLearning/w2LogisticRegressionAsNN/lr_utils.py
+++ NNandDeepLearning/w2LogisticRegressionAsNN/lr_utils.py
@@ -12,6 +12,7 @@ def load_dataset():
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    print("the list of classes", classes)
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
